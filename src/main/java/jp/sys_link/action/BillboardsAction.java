9d1795af56eaae333932cde12114a936df2c3e40
yoshi836 <y-toya@sys-link.jp>
2015-05-28 18:01:26 +0900

    社内掲示板アプリ　5/28日　アップロード処理

--- src/main/java/jp/sys_link/action/BillboardsAction.java
+++ src/main/java/jp/sys_link/action/BillboardsAction.java
@@ -1,5 +1,8 @@
 package jp.sys_link.action;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -10,8 +13,11 @@ import jp.sys_link.entity.Billboards;
 import jp.sys_link.entity.NameMst;
 import jp.sys_link.entity.Upfile;
 import jp.sys_link.form.BillboardsForm;
+import jp.sys_link.form.UpfileForm;
 import jp.sys_link.service.BillboardsService;
+import jp.sys_link.service.UpfileService;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.struts.upload.FormFile;
 import org.seasar.extension.jdbc.JdbcManager;
 import org.seasar.framework.beans.util.Beans;
@@ -24,14 +30,21 @@ public class BillboardsAction {
 	public List<Billboards> billboardsItems;
 	public List<NameMst> nameMstItems;
 	public Billboards billboardItems;
-	public Upfile upfile;
+	public List<Upfile> upfileItems;
 
 	@ActionForm
 	@Resource
 	private BillboardsForm billboardsForm;
 
+	@ActionForm
+	@Resource
+	private UpfileForm upfileForm;
+
 	@Resource
 	private BillboardsService billboardsService;
+
+	@Resource
+	private UpfileService upfileService;
 
 	@Resource
 	private JdbcManager jdbcManager;
@@ -66,6 +79,7 @@ public class BillboardsAction {
 
 	@Execute(validator = false, urlPattern = "edit/{id}")
 	public String edit() {
+		nameMstItems = jdbcManager.from(NameMst.class).getResultList();
 		Billboards entity = billboardsService.findById(Integer
 				.valueOf(billboardsForm.getId()));
 		Beans.copy(entity, billboardsForm).execute();
@@ -83,7 +97,9 @@ public class BillboardsAction {
 				.dateConverter("yyyy/MM/dd").execute();
 		billboardsService.insert(entity);
 
+		// TODO アップロード処理
 		upload(billboardsForm.getFormFile());
+
 		return "/billboards/";
 	}
 
@@ -102,12 +118,43 @@ public class BillboardsAction {
 				.createAndCopy(Billboards.class, billboardsForm)
 				.dateConverter("yyyy-MM-dd").execute();
 		billboardsService.delete(entity);
+
+		// TODO 上記で削除したIDが持っていたファイル情報の削除
+		upfileItems = jdbcManager.from(Upfile.class)
+				.where("billboardId = ?", entity.getId()).getResultList();
+		for (Upfile upfileEntity : upfileItems)
+			upfileService.delete(upfileEntity);
+
 		return "/billboards/";
 	}
 
 	protected void upload(FormFile file) {
-		// アップロードできているかの確認。
-		String path = application.getRealPath("/WEB-INF/" + file.getFileName());
-		UploadUtil.write(path, file);
+		/**
+		 * アップロードできているかの確認。 String path = application.getRealPath("/WEB-INF/" +
+		 * file.getFileName()); UploadUtil.write(path, file);
+		 */
+
+		// TODO アップロードしたファイル情報をデータベースに格納
+		//アップロードされたかどうか、ファイルネームで判定
+		if (file.getFileName() != "") {
+			String sql = "select * from billboards where id = (select Max(id) from billboards)";
+			Billboards entity = jdbcManager.selectBySql(Billboards.class, sql)
+					.getSingleResult();
+			try {
+				InputStream is = file.getInputStream();
+				upfileForm.setFile(IOUtils.toByteArray(is));
+				upfileForm.setBillboardId(entity.getId().toString());
+				upfileForm.setFileName(file.getFileName());
+
+				Upfile upfile = Beans.createAndCopy(Upfile.class, upfileForm)
+						.execute();
+				upfileService.insert(upfile);
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
